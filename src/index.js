52c2e1bf75f81abd1eede469320b040e8e113102
dikhorsun <dmitry.horsun@gmail.com>
2022-07-03 09:25:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+    const numberSmall = ['zero','one','two','three','four', 'five','six','seven','eight','nine', 
+    'ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+    const dozens = ['', 'ten', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    if(number > 999) {
+        return 'too big'
+    };
+    if(number < 20){
+        return numberSmall[number]
+    };
+    if(number <= 99){
+        if(number % 10 === 0) {
+            return dozens[number / 10]
+        } else {
+            return (dozens[Math.floor(number / 10)] + ' ' + numberSmall[number % 10])
+        }
+    };
+    if(number > 99){
+        if(number % 100 === 0){
+            return numberSmall[number / 100] + ' ' + 'hundred'
+        }
+        if(number % 10 === 0){
+            return numberSmall[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + dozens[(number % 100) / 10]
+        }
+        if((number % 100) < 20) {
+            return numberSmall[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + numberSmall[(number % 100)]
+        } else{
+            return numberSmall[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + dozens[Math.floor((number % 100) / 10)] + ' ' + numberSmall[(number % 100) % 10]
+        }
+    };
 }
